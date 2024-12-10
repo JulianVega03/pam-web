4124a366040a6cbe9a0248b6cc4ad7fed2160d15
Julian Becerra Vega <juabecer@bancolombia.com.co>
2024-12-10 04:03:32 -0500

    feat: securizando servicio de descarga reportes en excel

--- src/app/modules/inscription/services/excel.service.ts
+++ src/app/modules/inscription/services/excel.service.ts
@@ -1,22 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { JwtService } from '../../auth/services/jwt.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExcelService {
 
+  private httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: this._jwtService.getToken(),
+    }),
+    responseType: 'blob' as 'json'
+  };
+
   private apiUrl = `${environment.apiBaseUrl}/report/user/excel`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private _jwtService: JwtService) {}
 
-  downloadExcel(id: string): Observable<Blob> {
-    return this.http.get(`${this.apiUrl}/${id}`, { responseType: 'blob' });
+  public downloadExcel(id: string): Observable<Blob> {
+    return this.http.get<Blob>(`${this.apiUrl}/${id}`, this.httpOptions);
   }
 
-  downloadExcelAllAspirantes(): Observable<Blob> {
-    return this.http.get(`${this.apiUrl}/all`, { responseType: 'blob' });
+  public downloadExcelAllAspirantes(): Observable<Blob> {
+    return this.http.get<Blob>(`${this.apiUrl}/all`, this.httpOptions);
   }
 }
